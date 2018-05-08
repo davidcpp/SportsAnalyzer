14a9812452821422a0a8f31da7af08b23c3b40c2
Dawid Sroka <dawid190393@gmail.com>
2018-05-08 13:57:48 +0200

    Little changes in Stats view

--- SportsAnalyzer/App_Start/BundleConfig.cs
+++ SportsAnalyzer/App_Start/BundleConfig.cs
@@ -29,7 +29,7 @@ namespace SportsAnalyzer
 
       bundles.Add(new StyleBundle("~/Content/css").Include(
                 "~/Content/bootstrap.css",
-                "~/Content/site.css",
+                "~/Content/Site.css",
                 "~/Content/chartjs.css"));
     }
   }
