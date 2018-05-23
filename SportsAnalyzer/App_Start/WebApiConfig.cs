89a56a209dcbd7cb141d67b9919cd60020f13075
Dawid Sroka <dawid190393@gmail.com>
2018-05-23 10:54:12 +0200

    Add a Web API Controller - the Stats Controller
    This Controller provide football statistics data for views.

---
+++ SportsAnalyzer/App_Start/WebApiConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace SportsAnalyzer
+{
+  public static class WebApiConfig
+  {
+    public static void Register(HttpConfiguration config)
+    {
+      // TODO: Add any additional configuration code.
+
+      // Web API routes
+      config.MapHttpAttributeRoutes();
+
+      config.Routes.MapHttpRoute(
+          name: "DefaultApi",
+          routeTemplate: "Football/api/{controller}/{id}",
+          defaults: new { id = RouteParameter.Optional }
+      );
+    }
+  }
+}
---
+++ SportsAnalyzer/Controllers/StatsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SportsAnalyzer.Controllers
+{
+  public class StatsController : ApiController
+  {
+    static int[][] datasets = new int[][]
+    {
+      new int[] { 5, 15, 25, 30, 20, 5 },
+      new int[] { 7, 13, 27, 28, 18, 7 },
+      new int[] { 9, 11, 21, 34, 22, 3 }
+    };
+
+    [HttpGet]
+    public IHttpActionResult GetDataset(int id)
+    {
+      if (id >= 0 && id < 3)
+      {
+        return Ok(datasets[id]);
+      }
+      else
+      {
+        return NotFound();
+      }
+
+    }
+  }
+}
