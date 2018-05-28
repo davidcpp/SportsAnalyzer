b4d140524ec2546da06dec3a0df2532a7fe4ca91
Dawid Sroka <dawid190393@gmail.com>
2018-05-28 17:28:49 +0200

    Extract .js file from the Stats view

---
+++ SportsAnalyzer/Scripts/main.js
@@ -0,0 +1,286 @@
+﻿var paddingLeft, paddingRight, paddingTop, paddingBottom;
+var varTitleFontSize, ticksFontSize, legendFontSize, tooltipsFontSize, labelsFontSize;
+var timeIntervalsAllText = "";
+var timeIntervalsTexts = [];
+var goalsInIntervalsPercent = [];
+var webApiUri = 'api/stats';
+
+var myChartColors = [window.chartColors.blue,
+window.chartColors.red,
+window.chartColors.orange,
+window.chartColors.yellow];
+
+// Getting data of the Model passed from the Stats view
+
+//document.currentScript = document.currentScript || (function () {
+//  var scripts = $('script');
+//  return scripts[scripts.length - 1];
+//})();
+
+//var vars = document.currentScript.data();
+//console.log(vars);
+
+//timeIntervalsAllText = document.currentScript.data("timeIntervalsAllText");
+//goalsInIntervalsPercent[0] = document.currentScript.data("goalsInIntervalsPercent");
+
+//timeIntervalsAllText = $(document.currentScript).attr("timeIntervalsAllText");
+//goalsInIntervalsPercent[0] = $(document.currentScript).data("goalsInIntervalsPercent");
+
+timeIntervalsAllText = $("#mainScript").attr("data-time-intervals-all-text");
+goalsInIntervalsPercent[0] = eval($("#mainScript").attr("data-goals-in-intervals-percent"));
+
+
+function RemoveChartDataset(id) {
+  for (var i = 0; i < window.myChart.data.datasets.length; i++) {
+    if (window.myChart.data.datasets[i].label === 'Team ' + id) {
+      window.myChart.data.datasets.splice(i, 1);
+    }
+  }
+  window.myChart.update();
+}
+
+function UpdateChart(chart, chartDisplaySize) {
+  UpdateChartLayout(chart, chartDisplaySize);
+  UpdateChartFontSizes(chart, chartDisplaySize);
+  window.myChart.update();
+}
+
+function AddChartDataset(id) {
+  $.getJSON(webApiUri + '/' + id)
+    .done(function (data) {
+      goalsInIntervalsPercent[id] = data;
+
+      var dataset = {
+        label: 'Team ' + id,
+        backgroundColor: color(myChartColors[id]).alpha(0.5).rgbString(),
+        borderColor: myChartColors[id],
+        borderWidth: 1,
+        data: goalsInIntervalsPercent[id]
+      };
+
+      window.myChart.data.datasets.push(dataset);
+      window.myChart.update();
+    })
+    .fail(function (jqXHR, textStatus, err) {
+      console.log('Error: ' + err);
+    });
+}
+
+function GetChartDisplaySize() {
+  var canvasChart = document.getElementById('myChart');
+
+  chartDisplaySize = {
+    width: parseFloat(canvasChart.style.width),
+    height: parseFloat(canvasChart.style.height)
+  };
+  return chartDisplaySize;
+}
+
+function GetWindowWidth() {
+  var width = window.innerWidth
+    || document.documentElement.clientWidth
+    || document.body.clientWidth;
+  return width;
+}
+
+function GetWindowHeight() {
+  var height = window.innerHeight
+    || document.documentElement.clientHeight
+    || document.body.clientHeight;
+  return height;
+}
+
+function WriteWindowSizes() {
+  console.log("Browser inner window width: " + GetWindowWidth() + ", height: " + GetWindowHeight() + ".");
+}
+
+function UpdateChartLayout(chart, chartSize) {
+  paddingLeft = paddingRight = Math.round(0.04 * chartSize.width);
+  paddingTop = paddingBottom = Math.round(0.04 * chartSize.width);
+
+  if (typeof chart !== "undefined") {
+    chart.options.layout.padding = {
+      left: paddingLeft,
+      right: paddingRight,
+      top: paddingTop,
+      bottom: paddingBottom
+    };
+  }
+}
+
+function UpdateChartFontSizes(chart, chartSize) {
+  if (typeof chartSize !== "undefined") {
+    legendFontSize = Math.round(0.01 * chartSize.width + 8);
+    varTitleFontSize = Math.round(0.015 * chartSize.width + 8);
+    tooltipsFontSize = Math.round(0.005 * chartSize.width + 10);
+    labelsFontSize = Math.round(0.005 * chartSize.width + 10);
+    ticksFontSize = Math.round(0.005 * chartSize.width + 10);
+
+    console.log("Font sizes for chart - Variables values: ");
+    console.log([varTitleFontSize, ticksFontSize, legendFontSize, tooltipsFontSize, labelsFontSize].toString());
+  }
+
+  if (typeof chart !== "undefined") {
+    chart.options.title.fontSize = varTitleFontSize;
+    chart.options.legend.labels.fontSize = legendFontSize;
+    chart.options.tooltips.titleFontSize = tooltipsFontSize;
+    chart.options.tooltips.bodyFontSize = tooltipsFontSize;
+    chart.options.scales.yAxes[0].scaleLabel.fontSize = labelsFontSize;
+    chart.options.scales.xAxes[0].scaleLabel.fontSize = labelsFontSize;
+    chart.options.scales.yAxes[0].ticks.fontSize = ticksFontSize;
+    chart.options.scales.xAxes[0].ticks.fontSize = ticksFontSize;
+  }
+}
+
+function PrepareChartData() {
+  color = Chart.helpers.color;
+  timeIntervalsAllText = timeIntervalsAllText.substring(1, timeIntervalsAllText.length - 1);
+  timeIntervalsAllText = timeIntervalsAllText.replace(new RegExp('&quot;', 'g'), '');
+  timeIntervalsTexts = timeIntervalsAllText.split(",");
+}
+
+function OnResizeChart(chart, chartSize) {
+  WriteWindowSizes();
+  UpdateChart(chart, chartSize);
+}
+
+function CreateChart() {
+  PrepareChartData();
+
+  var ctx = $("#myChart");
+  //var ctx = document.getElementById('myChart').getContext('2d');
+  window.myChart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'bar',
+    // The data for our dataset
+    data: {
+      labels: timeIntervalsTexts,
+      datasets: [{
+        label: 'Scottish Premier League',
+        backgroundColor: color(myChartColors[0]).alpha(0.5).rgbString(),
+        borderColor: myChartColors[0],
+        borderWidth: 1,
+        data: goalsInIntervalsPercent[0]
+      }]
+    },
+    // Configuration options go here
+    options: {
+      responsive: true,
+      layout: {
+        padding: {
+          left: paddingLeft,
+          right: paddingRight,
+          top: paddingTop,
+          bottom: paddingBottom
+        }
+      },
+      legend: {
+        position: 'top',
+        labels: {
+          fontSize: legendFontSize
+        }
+      },
+      title: {
+        display: true,
+        fontSize: varTitleFontSize,
+        fontColor: window.chartColors.black,
+        text: 'Minutes Intervales of scored goals'
+      },
+      tooltips: {
+        titleFontSize: tooltipsFontSize,
+        bodyFontSize: tooltipsFontSize,
+        callbacks: {
+          label: function (tooltipItem, data) {
+            var label = data.datasets[tooltipItem.datasetIndex].label || '';
+
+            if (label) {
+              label += ': ';
+            }
+            label += tooltipItem.yLabel;
+            label += "%";
+            return label;
+          }
+        }
+      },
+      scales: {
+        yAxes: [{
+          scaleLabel: {
+            display: true,
+            labelString: 'Percent of goals',
+            fontColor: window.chartColors.blue,
+            fontSize: labelsFontSize
+          },
+          ticks: {
+            beginAtZero: true,
+            fontSize: ticksFontSize,
+            callback: function (value, index, values) {
+              return value + '%';
+            }
+          }
+        }],
+        xAxes: [{
+          scaleLabel: {
+            display: true,
+            labelString: 'Time intervals [min.]',
+            fontColor: window.chartColors.blue,
+            fontSize: labelsFontSize
+          },
+          ticks: {
+            fontSize: ticksFontSize
+          },
+          gridLines: {
+            offsetGridLines: true
+          }
+        }]
+      }
+    }
+  });
+  window.myChart.options.onResize = OnResizeChart;
+
+  chartDisplaySize = GetChartDisplaySize();
+  UpdateChart(window.myChart, chartDisplaySize);
+}
+
+$(document).ready(function () {
+  WriteWindowSizes();
+
+  //var scripts1 = $('script');
+  //var scripts2 = document.getElementsByTagName('script');
+  //console.log(scripts1);
+  //console.log(scripts2);
+  //console.log(document);
+  //console.log(document.currentScript);
+  //if (document.currentScript.async) {
+  //  console.log("Executing asynchronously");
+  //} else {
+  //  console.log("Executing synchronously");
+  //}
+  CreateChart();
+});
+
+$("#inlineCheckbox1").change(function () {
+  if ($(this).prop("checked")) {
+    AddChartDataset(1);
+  }
+  else {
+    RemoveChartDataset(1);
+  }
+});
+
+$("#inlineCheckbox2").change(function () {
+  if ($(this).prop("checked")) {
+    AddChartDataset(2);
+  }
+  else {
+    RemoveChartDataset(2);
+  }
+});
+
+$("#inlineCheckbox3").change(function () {
+  if ($(this).prop("checked")) {
+    AddChartDataset(3);
+  }
+  else {
+    RemoveChartDataset(3);
+  }
+});
